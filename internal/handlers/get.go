ac45811fbbc94d218c99754304e682f28d3f157a
NickolaySychev <sychev.nickolay@mail.ru>
2024-11-25 12:38:19 +0300

    catologize

---
+++ internal/handlers/get.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"net/http" // Для создания HTTP-сервера
+)
+
+// Функция getHandler обрабатывает GET-запросы на пути "/{id}".
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	// Извлечение id из URL пути.
+	id := r.URL.Path[1:]
+
+	// Поиск оригинального URL по id в карте.
+	mu.Lock()
+	originalURL, ok := urlMap[id]
+	mu.Unlock()
+
+	if !ok {
+		// Если id не найден, возвращаем ошибку 404 (Not Found).
+		http.NotFound(w, r)
+		return
+	}
+
+	// Установка заголовка Location и статуса 307 (Temporary Redirect).
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
